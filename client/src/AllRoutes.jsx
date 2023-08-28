5b49ca3535a06cba1ace439bf5be78b287682604
Varad Kulkarni <kulkarnivarad110@gmail.com.com>
2023-08-28 22:53:00 +0530

    Added components and pages

---
+++ client/src/AllRoutes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Home from './Pages/Home/Home'
+
+const AllRoutes = () => {
+  return (
+    <div className="all-routes">
+        <Home />
+    </div>
+  )
+}
+
+export default AllRoutes

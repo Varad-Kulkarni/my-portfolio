0ae4ab7b3bc5dbe9600a1a7b03ecebc05d5a91f0
Varad Kulkarni <kulkarnivarad110@gmail.com.com>
2023-08-29 22:53:24 +0530

    added pages and components and configured link settings

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
+import Projects from './Pages/Projects/Projects'
 
 const AllRoutes = () => {
   return (
-    <div className="all-routes">
-        <Home />
-    </div>
+    <Routes>
+      <Route path='/' element = { <Home/> }/>
+      <Route path='/about' element = { <About/> } />
+      <Route path='/projects' element = { <Projects/> } />
+    </Routes>
   )
 }
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import { Router } from 'react-router-dom'
 
+import Navbar from './components/Navbar/Navbar'
 import AllRoutes from './AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      <AllRoutes />
+        <Navbar />
+        <AllRoutes />
     </div>
   );
 }
